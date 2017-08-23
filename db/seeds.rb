bbd2005a9d94bdaf6b7b306c619125748ebec513
Chyld Medford <chyld.medford@gmail.com>
2017-08-23 21:04:13 +0000

    index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Car.delete_all
-Car.create(make: 'Toyota', model: 'Camry', year: 2015, vin: 'A0000000000000001')
-Car.create(make: 'Honda', model: 'Civic', year: 2014, vin: 'A0000000000000002')
-Car.create(make: 'Suburu', model: 'Outback', year: 2013, vin: 'A0000000000000003')
+Car.create!(make: 'Toyota', model: 'Camry', year: 2015, vin: 'A0000000000000001')
+Car.create!(make: 'Honda', model: 'Civic', year: 2014, vin: 'A0000000000000002')
+Car.create!(make: 'Suburu', model: 'Outback', year: 2013, vin: 'A0000000000000003')
